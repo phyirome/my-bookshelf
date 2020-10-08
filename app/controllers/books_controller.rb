b096db96b259fac7cb8a01db6db00f00d25e7675
phyirome <hirohiroxmt@gmail.com>
2020-10-09 03:59:27 +0900

    books#createアクションの定義

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,30 @@
 class BooksController < ApplicationController
-  def index
+
+  def mybooks
+    @books = Book.all.current_user.book
   end
 
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
   
+  def book_params
+    params.require(:book).permit(:title, :description, :author,:publisher).merge(user_id: current_user.id)
+  end
+
 end
