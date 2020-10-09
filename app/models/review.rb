60b7c8f1221d6d1a11a7e8f890e27a89f8c464d7
phyirome <hirohiroxmt@gmail.com>
2020-10-09 10:54:58 +0900

    reviewモデルとcommentモデルのアソシエーション定義

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
+  has_many :comments
+
+  validates :content, :rating, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   get 'books', to: 'books#mybooks' 
   devise_for :users
   root to: "books#index"
-  resources :books
+  resources :books do
+    resources :reviews do
+      resources :comments
+    end
+  end
 end
